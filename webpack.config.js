aa6243e0e7332ddece9f221031b45380a4338228
Craig Rice <craigricetong@gmail.com>
2020-01-27 19:59:10 -0800

    added less to webpack build

--- webpack.config.js
+++ webpack.config.js
@@ -26,6 +26,20 @@ module.exports = {
             use: ['style-loader', 'css-loader'],
           },
           {
+            test: /\.less$/,
+            use: [
+              {
+                loader: 'style-loader', // creates style nodes from JS strings
+              },
+              {
+                loader: 'css-loader', // translates CSS into CommonJS
+              },
+              {
+                loader: 'less-loader', // compiles Less to CSS
+              },
+            ],
+          },
+          {
             test: /\.html$/,
             use: [
               {
